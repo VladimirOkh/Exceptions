a61b5d4188e4ce768465b6cdbe6c08b53c8bc624
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2023-06-11 19:10:27 +0300

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,39 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.UnsupportedCharsetException;
 
+
+class ConvertException extends Exception {
+    ConvertException(String message) {
+        super(message);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        if (copyFileUsingStream("src/utf.txt", "UTF-8", "src/win1251.txt", "WINDOWS-1251")){
+        try {
+            copyFileUsingStream("src/utf8.txt", "UTF-8", "src/win1251.txt", "WINDOWS-1251");
             System.out.println("Перекодировка прошла успешно");
+        } catch (ConvertException e) {
+            System.out.println(e.getMessage());
         }
+
     }
 
-    private static boolean copyFileUsingStream(String source, String sourceEnc, String dest, String destEnc){
+    private static void copyFileUsingStream(String source, String sourceEnc, String dest, String destEnc) throws ConvertException {
         Charset sEnc = null;
-        try(Reader fis = new InputStreamReader(new FileInputStream(source), Charset.forName(sourceEnc));
-            Writer fos = new OutputStreamWriter(new FileOutputStream(dest), Charset.forName(destEnc));){
+        try (Reader fis = new InputStreamReader(new FileInputStream(source), Charset.forName(sourceEnc));
+             Writer fos = new OutputStreamWriter(new FileOutputStream(dest), Charset.forName(destEnc));) {
             char[] buffer = new char[1024];
             int length;
-            while((length = fis.read(buffer)) > 0) {
+            while ((length = fis.read(buffer)) > 0) {
                 fos.write(buffer, 0, length);
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Проблема с файлами: " + e.getMessage());
-            return false;
-        } catch (UnsupportedCharsetException e){
-            System.out.println("Проблема с кодировкой: " + e.getMessage());
-            return false;
-        } catch (IOException e){
-            System.out.println("Проблема с копированием");
-            return false;
+            throw new ConvertException("Проблема с файлами: " + e.getMessage());
+        } catch (UnsupportedCharsetException e) {
+            throw new ConvertException("Проблема с кодировкой: " + e.getMessage());
+        } catch (IOException e) {
+            throw new ConvertException("Проблема с копированием");
         }
-        return true;
     }
 }
