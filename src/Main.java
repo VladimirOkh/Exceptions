079c5405cfe197520b0d3fc793ed8060075f2a97
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2023-06-11 18:39:35 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private static boolean copyFileUsingStream(String source, String sourceEnc, String dest, String destEnc){
+        Charset sEnc = null;
+        try(Reader fis = new InputStreamReader(new FileInputStream(source), Charset.forName(sourceEnc));
+            Writer fos = new OutputStreamWriter(new FileOutputStream(dest), Charset.forName(destEnc));){
+            char[] buffer = new char[1024];
+            int length;
+            while((length = fis.read(buffer)) > 0) {
+                fos.write(buffer, 0, length);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Проблема с файлами: " + e.getMessage());
+            return false;
+        } catch (UnsupportedCharsetException e){
+            System.out.println("Проблема с кодировкой: " + e.getMessage());
+            return false;
+        } catch (IOException e){
+            System.out.println("Проблема с копированием");
+            return false;
+        }
+        return true;
+    }
+}
